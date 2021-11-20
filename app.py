9f6e5d1a594b756cfe0a974a537ba571ce6470d6
CountVariable <46840113+IamWafula@users.noreply.github.com>
2021-11-20 22:34:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -461,7 +461,8 @@ def shortened(route):
       link = get_link()
       set_dead(link,route)
 
-   bot = antibot()
+   #bot = antibot()
+   bot = False
 
    if bot:
       return redirect("https://youtu.be/dQw4w9WgXcQ")
